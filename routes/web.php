ffad2fce3996d93adda90c1fef92a3c9d5f0f5f8
ImtiyazAF <5026231197@student.its.ac.id>
2025-05-20 23:39:08 +0700

    pr pertemuan 11

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,27 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Link;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('page');
 });
 
-Route::get('/Halo', function () {
-    return ('Halo guyss wth');
+Route::get('/halo', [Link::class,'helloworld']);
+
+Route::get('/linktree', function () {
+	return view('linktree');
 });
 
-Route::get('/blog', function () {
-	return view('blog');
+Route::get('/js1', function () {
+	return view('js1');
 });
-
-Route::get('/haloob', [Link::class,'helloworld']);
 
 Route::get('/pertama', function () {
 	return view('pertama');
+});
+
+Route::get('/template1', function () {
+	return view('template1');
+});
+
+Route::get('/index', function () {
+	return view('index');
 });
