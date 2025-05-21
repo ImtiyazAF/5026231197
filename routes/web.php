a97dbfe2498a14b5243fe42bbe46a39a6c4df57d
ImtiyazAF <5026231197@student.its.ac.id>
2025-05-21 12:35:24 +0700

    pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Link;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
 
 Route::get('/', function () {
     return view('page');
 });
 
 Route::get('/halo', [Link::class,'helloworld']);
+
+Route::get('/dosen', [Link::class,'index']);
+
+Route::get('/pegawai/{nama}', [PegawaiController::class,'index']);
+
+Route::get('/formulir', [PegawaiController::class,'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class,'proses']);
+
+Route::get('/blog/home', [BlogController::class,'home']);
+Route::get('/blog/tentang', [BlogController::class,'tentang']);
+Route::get('/blog/kontak', [BlogController::class,'kontak']);
 
 Route::get('/linktree', function () {
 	return view('linktree');
